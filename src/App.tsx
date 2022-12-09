fe8ba1da3fcb0bcd43b10787ac5be1e36fa36c94
isaaclee12 <59675296+isaaclee12@users.noreply.github.com>
2022-12-08 22:55:54 -0500

    Added some styling Rollback point for map test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
-import bezos from'./assets/bezos.png';
+
+import bezos from './assets/bezos.png';
+// import bezos from'./assets/bezos_peek.png'; // TODO: Make this item mobile-only!
+
+import needs from './data/needs.json'
+
 import './App.css';
 
 function App() {
@@ -8,10 +13,7 @@ function App() {
   const [rent, setRent] = useState(0);
 
   const handleChange = (numberAdded: number, costPer: number, item: number, setItem: Function): void => {
-    // Multiply amount to change by cost per, and add to wealth
-    const amountToTakeFromWealth = numberAdded * costPer;
-
-    console.log(amountToTakeFromWealth);
+    const amountToTakeFromWealth = numberAdded * costPer;     // Multiply amount to change by cost per, and add to wealth
     
     // if we reduce an expense, we add to the wealth and reduce the expenses' count
     if (numberAdded === -1 && item > 0) {
@@ -31,22 +33,26 @@ function App() {
   return (
     <div className="App">
 
-      {/* HEADER: Header that has a title, no more no less */}
-      <section>
-        <header className="text-center">Become The Generous Jeff Bezos</header>
-      </section>
-
-      {/* SECTION 1:
+      {/* HEADER: Header that has a title, no more no less
+      SECTION 1:
       Stickied items:
       - Jeff Bezos' Face
       - A money meter that will stick to the top (TEST 1st) */}
-      <section className="sticky">
-        <div className="image-container">
-          <img src={bezos} alt="An image of Jeff Bezos' face" className="image"/>
+      <section className="flex-row-center">
+        <div className="sticky stretch">
+          <header className="text-center title">
+            <strong>Become The Generous Jeff Bezos</strong>  
+          </header>
+          
+          <div className="wealth-counter text-center">
+            <h3 className="">{wealth}</h3>
+          </div>
         </div>
-        <div className="wealth-counter text-center">
-          <h3>{wealth}</h3>
+
+        <div className="flex-row-center bezos-container">
+          <img src={bezos} alt="An image of Jeff Bezos' face" className="bezos-face"/>
         </div>
+
       </section>
 
       {/* SECTION 2:
@@ -56,17 +62,24 @@ function App() {
       2 cols on tablet
       1 col on mobile
       This splits the 8 items up nicely */}
-      <section>
-        <div className="expense-item text-center">
-          {/* insert image here */}
+      <section className="expense-list">
+
+        {
+          needs.map((need:{cost: number, image:string, item: string, setItem: string}) => {
+            <p>{need.item}</p>
+          })
+          
+        }
+
+        {/* <div className="expense-item text-center">
           <p>Rent</p>
           <div className="flex-row-center">
-            {/* if rent is 0, don't do anything, else decrease */}
             <button onClick={(e) => handleChange(-1, 7316, rent, setRent)}>-</button>
             <p>{rent}</p>
             <button onClick={(e) => handleChange(1, 7316, rent, setRent)}>+</button>
           </div>
-        </div>
+        </div> */}
+
       </section>
 
       {/* SECTION 3:
@@ -78,7 +91,8 @@ function App() {
         <a href="https://twitter.com/isaacwonhalee" target="_blank" className="icon brands alt fa-twitter"><span className="label">Twitter</span></a>
         <a href="https://www.linkedin.com/in/isaac-lee-621873133/" target="_blank" className="icon brands alt fa-linkedin"><span className="label">LinkedIn</span></a>
         <a href="https://github.com/isaaclee12" target="_blank" className="icon brands alt fa-github"><span className="label">GitHub</span></a>
-        <a href="mailto:isaac.wonha.lee@outlook.com" target="_blank" className="icon solid alt fa-envelope"><span className="label">Email</span></a>      </footer>
+        <a href="mailto:isaac.wonha.lee@outlook.com" target="_blank" className="icon solid alt fa-envelope"><span className="label">Email</span></a>
+      </footer>
     </div>
   );
 }
